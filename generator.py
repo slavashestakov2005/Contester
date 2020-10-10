d6f11b97da576f1935537c03b8c9a6f81a0c2e91
Слава <slavashestakov2005@gmail.com>
2020-10-10 20:32:22 +0700

    Добавлена компиляция. Частично добавлен запуск программы на тестах.

--- generator.py
+++ generator.py
@@ -22,7 +22,7 @@ head1 = r'''<%@ page language="java" contentType="text/html; charset=UTF-8"
 
 '''
 head2 = r'''<%@ page language="java" contentType="text/html; charset=UTF-8"
-         pageEncoding="UTF-8"%>
+         pageEncoding="Windows-1251"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 '''
--- src/com/example/Run.java
+++ src/com/example/Run.java
@@ -4,26 +4,42 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 @WebServlet("/run")
 public class Run extends HttpServlet {
-    private static final String commandStart = "cmd /c cd \"C:\\\\MinGW-5.1.0\\bin\" && g++ ";
+    private static final String root = "C:\\Users\\Public\\Documents\\Contester";
+    private static final String fileNameCpp = root + "\\Programs\\task.exe",
+                                fileNamePy = root + "\\Programs\\task.pyc",
+                                fileNameOutput = root + "\\Programs\\out.txt";
+    private static final String commandStart = "cmd /c ",
+            commandCpp = "g++ -static -fno-strict-aliasing -DACMP -lm -s -x c++ -std=c++17 -Wl,--stack=67108864 -O2 -o ",
+            commandPy1 = "python -c \"import py_compile; py_compile.compile(r'", commandPy2 = "',r'", commandPy3 = "')\"";
+    private static final String executeCpp = fileNameCpp + " > " + fileNameOutput,
+                                executePy = "python " + fileNamePy + " > " + fileNameOutput;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        /** get Parameters **/
         String name = request.getParameterValues("name")[0];
         String surname = request.getParameterValues("surname")[0];
         String code = request.getParameterValues("code")[0];
         String lang = request.getParameterValues("lang")[0];
         String path = request.getParameterValues("contest")[0] + "\\" + request.getParameterValues("task")[0];
+        /** save file **/
         System.out.println("POST запрос от " + name + " " + surname + " : " + lang);
-        saveFile(code, getFilePath(path) + "\\" + generateFileName(name, surname, lang));
-        // compileFile();
+        String fileName = getFilePath(path) + "\\" + generateFileName(name, surname, lang);
+        saveFile(code, fileName);
+        /** compile program **/
+        String command = getCompileCommand(fileName, lang);
+        System.out.println(command);
+        compileFile(command);
+        runProgram(path, lang);
     }
 
     protected String getFilePath(String path){
-        return "C:\\\\Users\\Слава\\Desktop\\Работы\\Контестер\\Пример\\" + path + "\\sendings";
+        return root + "\\Contests\\" + path + "\\sendings";
     }
 
     protected String generateFileName(String name, String surname, String lang){
@@ -33,14 +49,50 @@ public class Run extends HttpServlet {
         return fileName;
     }
 
-    protected void saveFile(String text, String file_name) throws IOException {
-        FileOutputStream out = new FileOutputStream(file_name);
+    protected void saveFile(String text, String fileName) throws IOException {
+        FileOutputStream out = new FileOutputStream(fileName);
         out.write(text.getBytes());
         out.close();
     }
 
-    protected void compileFile() throws IOException {
-        String command = commandStart + "-o \"C:\\\\Users\\Слава\\Desktop\\Работы\\Контестер\\program.exe\" \"C:\\\\Users\\Слава\\Desktop\\Работы\\Контестер\\code.cpp\"";
-        Runtime.getRuntime().exec(command);
+    protected String getCompileCommand(String fileName, String lang){
+        switch (lang){
+            case "cpp": return commandCpp + fileNameCpp + " \"" + fileName + "\"";
+            case "py": return commandPy1 + fileName + commandPy2 + fileNamePy + commandPy3;
+            default: return "";
+        }
+    }
+
+    protected void compileFile(String command) throws IOException {
+        try {
+            Runtime.getRuntime().exec(commandStart + command);
+        } catch (IOException ex){
+            System.out.println(ex);
+            ex.printStackTrace();
+        }
+    }
+
+    protected void runProgram(String path, String lang){
+        File[] files = new File(root + "\\Contests\\" + path + "\\tests").listFiles();
+        for(int i = 0; i < files.length; i += 2){
+            executeFile(files[i].getPath(), lang);
+        }
+    }
+
+    protected void executeFile(String input, String lang){
+        String command;
+        switch (lang){
+            case "cpp": command = executeCpp; break;
+            case "py": command = executePy; break;
+            default: command = "";
+        }
+        command += " < \"" + input + "\"";
+        System.out.println(command);
+        try {
+            Runtime.getRuntime().exec(commandStart + command);
+        } catch (IOException ex){
+            System.out.println(ex);
+            ex.printStackTrace();
+        }
     }
 }
